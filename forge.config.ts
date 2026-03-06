95ce3edd41f7d3689aa23f9bdaae557cfd63d6c8
compumike08 <compumike08@users.noreply.github.com>
2026-03-05 23:58:23 -0500

    Add GitHub actions publisher workflow

--- forge.config.ts
+++ forge.config.ts
@@ -22,6 +22,18 @@ const config: ForgeConfig = {
     new MakerRpm({}),
     new MakerDeb({}),
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'compumike08',
+          name: 'space-invaders-electron'
+        },
+        prerelease: false
+      }
+    }
+  ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
